8ca85dfbd69e4f0c7def4d75f17032f53c447ac2
sejal yadav <sejalyadav0818@gmail.com>
2023-09-05 12:31:49 +0530

    initial code

--- mern-crud/src/Users.jsx
+++ mern-crud/src/Users.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+
+const [users,setUsers] = useState("");
 
 const Users = () => {
   return (
-    <div>Users</div>
-  )
-}
+    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+      <div className="w-50 bg-white rounded p-3">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Age</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
 
-export default Users
+export default Users;
